12e9c84367e76e70c958f812e6b174e3cb7ec2c5
qaptivator <ac@i-ns.net>
2024-10-10 00:36:19 +0300

    done

--- src/midi_to_level.py
+++ src/midi_to_level.py
@@ -32,21 +32,21 @@ class TriggerArray:
         self.generate_array()
 
         for note in self.notes:
-            if note['variation'] and note['time']:
-                self.add_sound(note['time'], note['variation'], note['pitch'], note['volume'])
+            if note['time'] and note['variation']:
+                self.add_sound(note['time'], note['variation'])
 
     def generate_array(self):
-        for index, sound in enumerate(self.sounds):
+        for index, sound in enumerate(self.key_map):
             # if a note doesnt exist and it is compact mode, dont generate it
             if find(self.notes, 'variation', sound['t_variation']) or not self.compact:
-                column = index % self.sounds_per_row
-                row = math.floor(index / self.sounds_per_row)
+                column = index % self.keys_per_row
+                row = math.floor(index / self.keys_per_row)
                 pos = (column * self.trigger_padding[0] + self.trigger_offset[0], row * self.trigger_padding[1] + self.trigger_offset[1])
-                self.sounds_pos[sound['t_variation']] = pos
+                self.key_pos[sound['t_variation']] = pos
 
                 # regular collectable: ic 'io' {pos[0]} {pos[1]} 1 
                 # gravity collectable (nothing on the inside): ic 'im' {pos[0]} {pos[1]} 1  90 0
-                self.level_text += f'''ic 'im' {pos[0]} {pos[1]} 1  90 0
+                self.level += f'''ic 'im' {pos[0]} {pos[1]} 1  90 0
 trigger
 sfx \'{sound['t_variation']}\' {sound['t_volume']} {sound['t_pitch']} -1
 < {self.last_index}
@@ -64,16 +64,14 @@ sfx \'{sound['t_variation']}\' {sound['t_volume']} {sound['t_pitch']} -1
                 note_name = pretty_midi.note_number_to_name(note.pitch).upper()
                 if note_name:
                     #piano_note = list_safe_get(self.sounds, note_name)
-                    piano_note = find(self.sounds, 'midi_key', note_name)
+                    piano_note = find(self.key_map, 'midi_key', note_name)
                     if piano_note and piano_note['t_variation'] and note.start:
                         self.notes.append({
                             'variation': piano_note['t_variation'],
                             'time': note.start,
-                            'pitch': piano_note['t_pitch'] or 1,
-                            'volume': piano_note['t_volume'] or 1,
                         })
 
-    def add_sound(self, delay, variation, pitch=1, volume=1):
+    def add_sound(self, delay, variation):
         # Adds a generator (note) with specified delay and sound (paino key)
         # wait in between gen will be set to the midi's length, and the initial delay will be negative
         # tmc <x> <y> <radius> <density> <dissapear after * 60> <wait in between gen * 60> <initial delay * 60>
@@ -83,19 +81,19 @@ sfx \'{sound['t_variation']}\' {sound['t_volume']} {sound['t_pitch']} -1
         radius = 15
         dissapear_after = 0.05
 
-        pos = self.sounds_pos[variation]
+        pos = self.key_pos[variation]
         if pos:
-            self.level_text += f'''
+            self.level += f'''
 tmc {pos[0]} {pos[1]} {radius} 0 {dissapear_after * 60} {self.end_time * 60} {-(self.end_time - delay) * 60}
 noanim
 < {self.last_index}
 '''
             self.last_index += 1
         else:
-            print(f'position for variation \'{variation}\' does not exist, skipping adding sound')
+            print(f'Key for variation \'{variation}\' does not exist, skipping adding sound')
 
     def get_level(self):
-        return self.level_text
+        return self.level
         
 def main():
     parser = create_argparse(prog='midi_to_level', description='Generates music in a level by converting notes from a MIDI file into generators and triggers.')
@@ -117,9 +115,6 @@ def main():
         return
 
     trigger_array = TriggerArray(level, midi, args.compact, args.speed)
-    trigger_array.generate_array()
-    trigger_array.extract_notes()
-    trigger_array.add_sounds_from_midi()
     level_text = trigger_array.get_level()
 
     level_output(level_text, args)
